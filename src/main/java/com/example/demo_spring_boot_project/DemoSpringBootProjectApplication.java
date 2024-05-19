29c9836d111cb3b3c7512df0e8b9d5d681196a82
unknown <mustafo>
2024-05-19 13:39:23 +0500

    Initial commit

---
+++ src/main/java/com/example/demo_spring_boot_project/DemoSpringBootProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo_spring_boot_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringBootProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoSpringBootProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/demo_spring_boot_project/DemoSpringBootProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo_spring_boot_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringBootProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
